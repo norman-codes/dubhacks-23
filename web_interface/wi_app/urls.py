aed82db81f8b57fa727edf1cf535b05d07d6810e
norman-codes <norjul17@gmail.com>
2023-10-14 18:04:36 -0700

    Implemented step 6 of Django/Ajax tutorial for voice recording input.
    (ref: https://dev.to/sirneij/django-and-ajax-building-a-recording-application-4j0a)

--- web_interface/wi_app/urls.py
+++ web_interface/wi_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = "wi_app"
 
 urlpatterns = [
-    path("", views.index, name="index") # index path
+    path("", views.index, name="index"), # index path
+    path("recording/", views.recording, name="recording"),
+    path("record/detail/<uuid:id>", views.recording_detail, name="recording_detail")
 ]
