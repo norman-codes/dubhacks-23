a29ecfcbec5525e16ca3c9d9a335266dcef361a1
norman-codes <norjul17@gmail.com>
2023-10-15 01:40:44 -0700

    Added firebase cloud storage for recording upload

--- web-interface/src/AudioRecorder.tsx
+++ web-interface/src/AudioRecorder.tsx
@@ -1,16 +1,12 @@
-/*
-  Referenced from the following GitHub repository:
-  - https://github.com/samhirtarif/react-audio-visualize
-  - https://stackblitz.com/edit/stackblitz-starters-kjpu5q?file=src%2FApp.tsx
-*/
-import { error } from 'console';
 import * as React from 'react';
 import { useState } from 'react';
 import { AudioVisualizer, LiveAudioVisualizer } from 'react-audio-visualize';
 import { AudioRecorder, useAudioRecorder } from 'react-audio-voice-recorder';
+import { ref, uploadBytes } from 'firebase/storage';
+import { storage } from './firebase';  // Ensure firebase.js is in the same directory and properly configured
 
 export default function AudioInputAndDisplay() {
-  const [blob, setBlob] = useState<Blob>();
+  const [blob, setBlob] = useState<Blob | null>(null);
   const recorder = useAudioRecorder(
     {
       noiseSuppression: true,
@@ -19,13 +15,42 @@ export default function AudioInputAndDisplay() {
     (error) => console.table(error) // onNotAllowedOrFound
   );
 
-  const addAudioElement = (blob: Blob) => {
-    const url = URL.createObjectURL(blob);
-    const audio = document.createElement('audio');
-    audio.src = url;
-    audio.controls = true;
-    document.body.appendChild(audio);
+  const addAudioElement = (blob: Blob | null) => {
+    if (blob) {
+      const url = URL.createObjectURL(blob);
+      const audio = document.createElement('audio');
+      audio.src = url;
+      audio.controls = true;
+      document.body.appendChild(audio);
+    }
   }
+
+  const saveAudio = (blob: Blob | null) => {
+    if (blob) {
+      // Get the current date and time
+      const now = new Date();
+  
+      // Format the date and time components so that they have leading zeros if necessary
+      const year = now.getFullYear();
+      const month = String(now.getMonth() + 1).padStart(2, '0');  // Months are 0-based, so add 1
+      const day = String(now.getDate()).padStart(2, '0');
+      const hour = String(now.getHours()).padStart(2, '0');
+      const minute = String(now.getMinutes()).padStart(2, '0');
+      const second = String(now.getSeconds()).padStart(2, '0');
+  
+      // Combine the date and time components to form the file name
+      const fileName = `${month}${day}${year}${hour}${minute}${second}.wav`;
+  
+      // Create a reference to the file in Firebase Cloud Storage
+      const audioRef = ref(storage, `audio/${fileName}`);
+  
+      // Upload the blob
+      uploadBytes(audioRef, blob).then((snapshot) => {
+        console.log('Uploaded a blob or file!');
+      });
+    }
+  };
+  
 
   return (
     <div>
@@ -57,7 +82,7 @@ export default function AudioInputAndDisplay() {
       )}
 
       <button onClick={() => addAudioElement(blob)}>Add Audio Element</button>
-
+      <button onClick={() => saveAudio(blob)}>Save Audio</button>
     </div>
   );
 }
---
+++ web-interface/src/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// edit firebaseCOnfig with new .env names
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
+export { storage };
